f75bb5bdc39dcfb58b4fc09fb58c768321f62f19
emdodd123 <emdodd123@gmail.com>
2024-01-29 10:29:12 -0500

    Add sprint3 files and sprint2 after HW

---
+++ sprint3/add.py
@@ -0,0 +1,11 @@
+# Input the first number
+num1 = int(input("Enter the first number: "))
+
+# Input the second number
+num2 = int(input("Enter the second number: "))
+
+# Add the numbers
+sum_result = num1 + num2
+
+# Display the result
+print(f"The sum of {num1} and {num2} is: {sum_result}")
